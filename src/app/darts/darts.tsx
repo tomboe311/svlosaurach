ae0dff9da5a6267d7e37c0c3d91426e657701dac
Thomas Boehringer <dev@tboehringer.de>
2025-05-16 14:29:42 +0200

    update text for "Dart"

--- src/app/darts/darts.tsx
+++ src/app/darts/darts.tsx
@@ -47,14 +47,13 @@ function Darts() {
                 </Typography>
                 <Typography variant="lead" className="mb-4 !text-gray-500 md:pr-16 xl:pr-28">
                     Am 25.03.2023 wurde auf der Jahreshauptversammlung des SV Losaurach e.V. die Abteilung
-                    Dart neu gegründet. Mittlerweile, bis zum heutigen Datum (11.12.2024), zählen über 70
-                    begeisterte Mitglieder zu den „Dartern“. Sebastian Klein leitet die Abteilung, während
-                    Christian Böhm als Kapitän die Mannschaft führt.
+                    Dart neu gegründet. Mittlerweile zählen über 70 begeisterte Mitglieder zu den „Dartern“.
+                    Sebastian Klein leitet die Abteilung, während Christian Böhm als Kapitän die Mannschaft führt.
                 </Typography>
                 <Typography variant="lead" className="mb-4 !text-gray-500 md:pr-16 xl:pr-28">
-                    Derzeit nimmt eine Mannschaft aktiv am Spielbetrieb teil: „Los Dartos“. In der ersten Saison
-                    2024 konnte das Team mit einer beeindruckenden Leistung den vierten Platz in der B-Klasse
-                    7 (Nürnberg/Fürth) erreichen und somit die Klasse halten.
+                    Derzeit nimmt eine Mannschaft aktiv am Spielbetrieb teil: „Los Dartos“. In der ersten Saison 2024
+                    konnte das Team mit einer beeindruckenden Leistung den vierten Platz in der B-Klasse 7
+                    (Nürnberg/Fürth) erreichen und somit die Klasse halten.
                 </Typography>
                 <Typography variant="lead" className="mb-4 !text-gray-500 md:pr-16 xl:pr-28">
                     Als Trainingsstätte dient der Saal des Sportheims Losaurach, wo inzwischen drei moderne E-
@@ -64,23 +63,17 @@ function Darts() {
                 <Typography variant="lead" className="mb-4 !text-gray-500 md:pr-16 xl:pr-28">
                     Die Idee zur Gründung einer eigenen Abteilung Dart entstand bereits vor vielen Jahren, als
                     noch im Losauracher „Hüttla“ gespickert wurde. Der Gedanke wurde durch unser LA-Dart
-                    Champion Turnier weitergetragen, das im kommenden Jahr bereits zum achten Mal
-                    stattfindet. Dieses Turnier gehört zu den größten Events des Sportvereins: Über 60
-                    Teilnehmer treten auf fünf Scheiben im Schützenkeller gegeneinander an, um schließlich
-                    einen Sieger zu küren. Dabei stehen vor allem der Spaß und die Freude am gemeinsamen
-                    Spiel im Vordergrund.
-                </Typography>
-                <Typography variant="lead" className="mb-4 !text-gray-500 md:pr-16 xl:pr-28">
-                    Auch dieses Jahr heißt es wieder: „Wer checkt am schnellsten aus?“
-                    LA-Dart Champion Turnier 2025
-                    15. Februar 2025
+                    Champion Turnier weitergetragen, das jährlich im Februar stattfindet. Dieses Turnier gehört
+                    zu den größten Events des Sportvereins. Um die 60 Teilnehmer treten auf fünf Scheiben im
+                    Schützenkeller gegeneinander an, um schließlich einen Sieger zu küren. Dabei stehen vor allem
+                    der Spaß und die Freude am gemeinsamen Spiel im Vordergrund.
                 </Typography>
                 <Typography variant="lead" className="mb-4 !text-gray-500 md:pr-16 xl:pr-28">
                     Haben wir euer Interesse geweckt?
                     Dann seid ihr herzlich eingeladen, mittwochs ab 18 Uhr unter Rücksprache mit unserem
-                    Spielführer Christian Böhm zu einem Schnuppertraining
-                    vorbeizukommen. Ihr müsst nicht einmal eigene Darts mitbringen – wir stellen euch gerne
-                    welche zum Testen zur Verfügung.
+                    Spielführer Christian Böhm zu einem Schnuppertraining vorbeizukommen.
+                    Ihr müsst nicht einmal eigene Darts mitbringen – wir stellen euch gerne welche zum
+                    Testen zur Verfügung.
                 </Typography>
                 <Typography variant="lead" className="mb-4 !text-gray-500 md:pr-16 xl:pr-28">
                     Egal, ob ihr schon Erfahrung habt oder absolute Anfänger seid: Bei uns ist jeder
@@ -89,7 +82,7 @@ function Darts() {
                 <img
                     className="min-h-full w-full rounded-lg object-cover object-center"
                     src="/teams/sv-losaurach-dart.jpeg"
-                    alt="Herrenmannschaft"
+                    alt="SV Losaurach - Los Dartos"
                 />
             </div>
             <div className="container mx-auto">
